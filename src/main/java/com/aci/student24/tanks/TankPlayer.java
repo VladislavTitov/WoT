94b9d4a0ce1bf3c5212184774fd0dc7ae9e8ce65
Ruslanio <mirhusainow@gmaol.com>
2017-11-25 11:46:33 +0300

    base initialization

--- src/main/java/com/aci/student24/tanks/TankPlayer.java
+++ src/main/java/com/aci/student24/tanks/TankPlayer.java
@@ -4,20 +4,46 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import com.aci.student24.api.tanks.Algorithm;
+import com.aci.student24.api.tanks.objects.Base;
 import com.aci.student24.api.tanks.state.MapState;
 import com.aci.student24.api.tanks.state.TankMove;
 
 public class TankPlayer implements Algorithm {
-  private int teamId;
+    private int teamId;
+    private Base enemyBase;
+    private Base ourBase;
+    private byte ourDislocation;
+    private int count = 0;
 
-  @Override
-  public void setMyId(final int id) {
-    teamId = id;
-  }
 
-  @Override
-  public List<TankMove> nextMoves(MapState mapState) {
-    return mapState.getTanks(teamId).stream()
-        .map(tank -> new TankMove(tank.getId(), tank.getDir(), true)).collect(Collectors.toList());
-  }
+    @Override
+    public void setMyId(final int id) {
+        teamId = id;
+    }
+
+    @Override
+    public List<TankMove> nextMoves(MapState mapState) {
+        if (count == 0)
+            initBaseParametrs(mapState);
+        count++;
+        return null;
+    }
+
+    private void initBaseParametrs(MapState mapState) {
+        mapState.getBases().forEach(base -> {
+            if (base.getId() == teamId)
+                ourBase = base;
+            else
+                enemyBase = base;
+        });
+        if (ourBase.getX() > enemyBase.getX())
+            ourDislocation = Location.RIGHT_DISLOCATION;
+        else
+            ourDislocation = Location.LEFT_DISLOCATION;
+    }
+}
+
+class Location {
+    public static final byte LEFT_DISLOCATION = 0;
+    public static final byte RIGHT_DISLOCATION = 1;
 }
